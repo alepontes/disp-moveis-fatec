482ffdc87e330e32826f66b19199e041b84d6930
alepontes <alexandrelopespontes@gmail.com>
2019-09-11 08:49:11 -0300

    Finalizar DAO

--- src/DAO/PlaceDAO.java
+++ src/DAO/PlaceDAO.java
@@ -3,36 +3,70 @@ package DAO;
 import MODEL.Place;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class PlaceDAO {
 
-    private final Connection c;
+    private final Connection connection;
 
     public PlaceDAO() throws SQLException, ClassNotFoundException, SQLException {
-        this.c = new DAO().getConnection();
+        this.connection = new DAO().getConnection();
     }
 
+    public Place getById(String idPlace) throws SQLException {
 
-    public Place getById(String idPlace){
-        return new Place();
+        String sql = "select * from place WHERE id = ?";
+        PreparedStatement stmt = this.connection.prepareStatement(sql);
+
+        stmt.setInt(1, Integer.parseInt(idPlace));
+        ResultSet rs = stmt.executeQuery();
+
+        Place place = new Place();
+        while (rs.next()) {
+            place.setId(String.valueOf(rs.getInt(1)));
+            place.setName(rs.getString(2));
+            place.setDescription(rs.getString(3));
+        }
+
+        stmt.close();
+
+        return place;
     }
 
-    public Place[] list(){
-        return new Place[0];
+    public ArrayList<Place> list() throws SQLException {
+
+        ArrayList<Place> places = new ArrayList<Place>();
+        Place place = new Place();
+
+        String sql = "select * from place";
+        PreparedStatement stmt = this.connection.prepareStatement(sql);
+
+        ResultSet rs = stmt.executeQuery();
+
+        while (rs.next()) {
+            place.setId(String.valueOf(rs.getInt(1)));
+            place.setName(rs.getString(2));
+            place.setDescription(rs.getString(3));
+
+            places.add(place);
+        }
+
+        rs.close();
+        stmt.close();
+
+        return places;
+
     }
 
     public Place add(Place place) throws SQLException {
 
         String sql = "insert into place" + " (name, description)" + " values (?,?)";
 
-        // prepared statement para inserção
-        PreparedStatement stmt = c.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
-        // seta os valores
-        stmt.setString(2, place.getName());
-        stmt.setString(3, place.getDescription());
+        stmt.setString(1, place.getName());
+        stmt.setString(2, place.getDescription());
 
-        // executa
         stmt.executeUpdate();
         ResultSet rs = stmt.getGeneratedKeys();
 
@@ -46,12 +80,45 @@ public class PlaceDAO {
 
     }
 
-    public boolean remove(Place place){
+    public boolean remove(int idPlace) {
+
+        String sql = "delete from place where id = ?";
+
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, idPlace);
+            stmt.execute();
+            stmt.close();
+            connection.close();
+        } catch (SQLException e) {
+            return false;
+            // e.printStackTrace();
+        }
+
         return true;
     }
 
-    public Place updade(Place place){
-        return new Place();
+
+    public Place updade(Place place) {
+
+        String sql = "update place SET name = ?, description = ? WHERE id = ?";
+
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, place.getName());
+            stmt.setString(2, place.getDescription());
+            stmt.setInt(3, Integer.parseInt(place.getId()));
+
+            stmt.execute();
+            stmt.close();
+
+            return place;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 
 }
